1f3e9a7e15f604c27eececb5817ceba6874691ef
Mrtly <mrtly.ca@gmail.com>
2020-08-29 10:19:58 -0700

    hello

--- hello.py
+++ hello.py
@@ -2,10 +2,16 @@
 
 import platform
 
+x = 42
+hello = 'hello world. {}'.format(x)
+
 def main():
     message()
 
 def message():
     print('This is python version {}'.format(platform.python_version()))
     print('woohoo Python yay!')
+    print('hello world. {}'.format(x))
+    print(hello)
+    print(f'hello world. {x}')
 if __name__ == '__main__': main()
