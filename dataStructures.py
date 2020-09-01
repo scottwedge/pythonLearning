9b28def0af5bea00d8e7b4a7b0ebcdb6ac958d39
Mrtly <mrtly.ca@gmail.com>
2020-08-31 17:38:37 -0700

    data structures

---
+++ dataStructures.py
@@ -0,0 +1,14 @@
+# list -> mutable
+x = [ 1, 2, 3, 4, 5]
+
+# tuple -> immutable
+x = ( 1, 2, 3, 4, 5 )
+
+# dictionary (key-value pairs, like associative/hash array)
+x = { "a": 1, "b: 2, "c": 3 }
+
+# set -> unordered list of unique values
+x = { 1, 2, 3, 4, 5}
+
+# these structures can contain any object, 
+# and can be used to represent simple or complex structued data
