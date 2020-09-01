181c4013e68c70505c9ab955f9de78fd83d52202
Mrtly <mrtly.ca@gmail.com>
2020-08-31 17:38:27 -0700

    constructor

---
+++ constructor.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+class Animal:
+    def __init__(self, **kwargs):
+        self._type = kwargs['type'] if 'type' in kwargs else 'kitten'
+        self._name = kwargs['name'] if 'name' in kwargs else 'fluffy'
+        self._sound = kwargs['sound'] if 'sound' in kwargs else 'mew'
+
+    def type(self, t = None):
+        if t: self._type = t
+        return self._type
+
+    def name(self, n = None):
+        if n: self._name = n
+        return self._name
+
+    def sound(self, s = None):
+        if s: self._sound = s
+        return self._sound
+
+def print_animal(o):
+    if not isinstance(o, Animal):
+        raise TypeError('print_animal(): requires an Animal')
+    print('The {} is named "{}" and says "{}".'.format(o.type(), o.name(), o.sound()))
+
+
+def main():
+    a0 = Animal(type= 'kitten', name= 'fluffy', sound= 'rwar')
+    a1 = Animal(type= 'duck', name= 'donald', sound= 'quack')
+    print_animal(a0)
+    print_animal(a1)
+    print_animal(Animal(type= 'velociraptor', name= 'veronica', sound= 'hello'))
+
+if __name__ == '__main__': main()
